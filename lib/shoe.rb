49c685b614feac7b9b38347e128e59abb38b26a3
Mariam Nersisyan <mariamnersisyan97@gmail.com>
2022-06-30 15:00:53 -0400

    commit tree e87079309c03484ffd5c1cb14575d2306acac016

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,29 @@
-# Make your shoe class here!
+require 'pry'
+class Shoe
+    attr_reader :brand, :color, :size, :material, :condition
+    attr_accessor :condition
+    def initialize(brand)
+        @brand = brand
+    end
+    # def brand=(brand)
+    #     @brand = brand
+    # end
+    def color=(color)
+        @color = color
+    end
+    def size=(size)
+        @size = size
+    end
+    def material=(material)
+        @material = material
+    end
+    def condition=(condition)
+        @condition = condition
+    end
+    def cobble
+        puts  "Your shoe is as good as new!"
+        @condition = "new"
+    end
+end
+
+
